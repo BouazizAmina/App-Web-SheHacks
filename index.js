25e39657b5c4dbc46e85a33975e3533e56e98fa9
BouazizAmina <ia_bouaziz@esi.dz>
2022-05-13 23:50:31 +0100

    fixe errors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const http = require('http');
 const port = 8083;
 
 var app = express()
+
 app.get("/",async function(req,res,next){
     res.sendfile(__dirname + '/index.html');
 })
@@ -15,10 +16,11 @@ app.listen(8083, function () {
 console.log("Started application on port %d", 8083)
 });
 app.get("/sleep",async function(req,res,next){
-    res.sendfile(__dirname + '/sleep.html');
+    res.sendfile(__dirname + '/sleep.html');console,console.log(success);
 })
 app.get("/changeVideo",async function(req,res,next){
     res.sendfile(__dirname + '/changeVideo.html');
 })
 
+app.use(express.static(__dirname + '/public'));
 
